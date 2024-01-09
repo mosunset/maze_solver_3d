8aff6c51ab1ad5150409476ab9c00a2855373d12
drivepc <driveasano@gmail.com>
2024-01-09 23:41:15 +0900

    9

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
   assetPrefix: prefixPath,
   basePath: prefixPath,
   reactStrictMode: true,
+  trailingSlash: true,
   output: "export",
   images: {
     unoptimized: true,
