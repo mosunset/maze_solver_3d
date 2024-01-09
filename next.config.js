dd225fe88132c5ebcae834dc112a42fdee78319f
drivepc <driveasano@gmail.com>
2024-01-09 23:35:12 +0900

    7

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV === 'development'
+console.log(process.env.NODE_ENV)
 
-const isProd = process.env.NODE_ENV === 'production'
-const prefixPath = !isProd ? '/260277p/maze_solver_3d' : ''
+/* 公開時のサブディレクトリ */
+const SUB_DIRECTORY = "/260277p/maze_solver_3d";
+const prefixPath = !isProd ? SUB_DIRECTORY : ''
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: prefixPath,
   basePath: prefixPath,
   reactStrictMode: true,
-  output: "standalone",
+  output: "export",
   images: {
     unoptimized: true,
   },
